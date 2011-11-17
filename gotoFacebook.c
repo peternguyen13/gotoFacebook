b4e1b05b5075fd7a61caf8ce133c55b6501160aa
Peter Nguyen <hoangcntt13@gmail.com>
2011-11-17 22:23:35 +0700

    Third Commit

--- gotoFacebook.c
+++ gotoFacebook.c
@@ -1,41 +1,33 @@
+/** **************** **/
+/** gotoFacebook.c **/
+
+
 /** gotoFacebook Program
- * Version 0.3a
+ * Version 0.4a
  * Written by Nguyen Vu Hoang (Peter Nguyen)
  * Public: GPL
  **/
 
-/**
- * 1. How to build gotoFacebook?
- * 	+For Windows:
- * 		Uses: Codeblocks, C-Free, Dev-C to build
- * 	+For Linux:
- * 		Uses: gcc gotoFacebook.c -o gotoFacebook
- * 2. Using gotoFacebook:
- * 	+For Windows:
- * 		Run Command Line as Administrator
- * 		Run gotoFacebook < list file include ip to hosts >
- * 	+For Linux:
- * 		The same
- * 		Run as root
- **/ 
-
+/** check system **/
+#if defined(_WIN32) || defined(WIN32) || defined(__CYGWIN__) || defined(__MINGW32__) || defined(__BORLANDC__)
+  static char link_host[255]="C:\Windows\System32\driver\etc\hosts";  // for windows
+#else
+  static char link_host[255]="/etc/hosts"; // for Linux
+#endif
+/** end check system **/
 #include<stdio.h>
 #include<unistd.h>
 #include<stdlib.h>
 #define MAX_LINE_LEN 1000
 
-static char link_host[MAX_LINE_LEN+1]="/etc/hosts"; // => For Linux
-static char backup_default[MAX_LINE_LEN+1]="127.0.0.1 localhost"; //=> change file this to be suitable for you
-// For Windows: static char link_host[MAX]="C:\Windows\System32\driver\etc\hosts"
-
 /*Change host file*/
-int changes_host(char link_host_files[MAX_LINE_LEN+1]){  
+int changes_host(char link_list[MAX_LINE_LEN+1]){  
   char buf[MAX_LINE_LEN+1];
   FILE *file_from;
   FILE *file_to;
-  file_from=fopen(link_host_files,"r");  
+  file_from=fopen(link_list,"r");  
   if (!file_from){//loi mo file
-    fprintf(stderr,"Cannot Open File %s",link_host_files);
+    fprintf(stderr,"Cannot Open File %s",link_list);
     perror("");
     return 0;
   }
@@ -49,7 +41,7 @@ int changes_host(char link_host_files[MAX_LINE_LEN+1]){
   //write file
   while(fgets(buf,MAX_LINE_LEN+1,file_from)){
     if(fputs(buf,file_to)==EOF){
-      fprintf(stderr,"Error writting to target file\n");
+      fprintf(stderr,"Error writting file\n");
       perror("");
       return 0;
     }
@@ -72,18 +64,91 @@ int changes_host(char link_host_files[MAX_LINE_LEN+1]){
   return 1;
 }
 /*Resore host file*/
-int restore_host_file(){
-  FILE *f_open;
+int restore_host_file(char link_to_restore[MAX_LINE_LEN]){
+  char buf[MAX_LINE_LEN+1];
+  FILE *file_from;
+  FILE *file_to;
   
-  f_open=fopen(link_host,"w+");
-  if (!f_open){
+  file_from=fopen(link_to_restore,"r");
+  if (!file_from){
     fprintf(stderr,"Error Open file %s \n",link_host);
     perror("");
     return 0;
   }
-  //Restore
-  fprintf(f_open,backup_default);
-  if (fclose(f_open)){
+  
+  file_to=fopen(link_host,"w+");
+  if (!file_to){
+    fprintf(stderr,"Error Open file %s \n",link_host);
+    perror("");
+    return 0;
+  }
+  //chep file
+  while(fgets(buf,MAX_LINE_LEN+1,file_from)){
+    if(fputs(buf,file_to)==EOF){
+      fprintf(stderr,"Error writting file\n");
+      perror("");
+      return 0;
+    }
+  }
+  
+  if(!feof(file_from)){
+    fprintf(stderr,"Error reading sources file\n");
+    perror("");
+    return 0;
+  }
+  
+  if (fclose(file_from)){
+    fprintf(stderr,"Error close file %s \n",link_to_restore);
+    perror("");
+  }
+  
+  if (fclose(file_to)){
+    fprintf(stderr,"Error close file %s \n",link_host);
+    perror("");
+  }
+  return 1;  
+}
+
+
+int backup_host_file(char link_to_backup[MAX_LINE_LEN+1]){
+  char buf[MAX_LINE_LEN+1];
+  FILE *file_from;
+  FILE *file_to;
+  
+  file_from=fopen(link_host,"r");
+  if (!file_from){
+    fprintf(stderr,"Error Open file %s \n",link_host);
+    perror("");
+    return 0;
+  }
+  
+  file_to=fopen(link_to_backup,"w+");
+  if (!file_to){
+    fprintf(stderr,"Error Open file %s \n",link_to_backup);
+    perror("");
+    return 0;
+  }
+  //chep file
+  while(fgets(buf,MAX_LINE_LEN+1,file_from)){
+    if(fputs(buf,file_to)==EOF){
+      fprintf(stderr,"Error writting file\n");
+      perror("");
+      return 0;
+    }
+  } 
+  
+  if(!feof(file_from)){
+    fprintf(stderr,"Error reading sources file\n");
+    perror("");
+    return 0;
+  }
+  
+  if (fclose(file_from)){
+    fprintf(stderr,"Error close file %s \n",link_to_backup);
+    perror("");
+  }
+  
+  if (fclose(file_to)){
     fprintf(stderr,"Error close file %s \n",link_host);
     perror("");
   }
@@ -93,28 +158,37 @@ int restore_host_file(){
 /* Main program */
 int main(int argc, char *argv[]){
   int opt;//getopt()
-  if (argc < 1){
+  if (argc < 2){
     fprintf(stderr,"Usage:%s <option> <sources of file>\n",argv[0]);
-    fprintf(stdout,"Option:\n");
-    fprintf(stdout,"-U: update list file\n");
-    fprintf(stdout,"-R: restore list file\n");
-    fprintf(stdout,"-B: backup list file\n");
+    fprintf(stdout,"Use commant %s -H to help",argv[0]);
   }
   else{
-    while ((opt=getopt(argc,argv,"u:r"))!=-1){
+    while ((opt=getopt(argc,argv,"U:B:R:H"))!=-1){
       switch(opt){
-	case 'u':
+	case 'U':
 	  if (changes_host(optarg))
 	    fprintf(stdout,"%s has been changed successfully. \n",optarg);
 	  else
 	    fprintf(stdout,"%s hasn't been changed.\n",optarg);
 	  break;
-	case 'r':
-	  if (restore_host_file())
+	case 'R':
+	  if (restore_host_file(optarg))
 	    fprintf(stdout,"File has been restored successfully. \n");
 	  else
 	    fprintf(stdout,"File hasn't been restored.\n");
-	  break;      
+	  break;
+	case 'B':
+	  if (backup_host_file(optarg))
+	    fprintf(stdout,"File has been restored successfully. \n");
+	  else
+	    fprintf(stdout,"File hasn't been restored.\n");
+	  break;
+	case 'H':
+	  fprintf(stdout,"Option:\n");
+	  fprintf(stdout,"-U: update list file\n");
+	  fprintf(stdout,"-R: restore list file\n");
+	  fprintf(stdout,"-B: backup list file\n");
+	  break;
 	case '?':
 	  fprintf(stdout,"Unknown option %s",optopt);
 	  break;
